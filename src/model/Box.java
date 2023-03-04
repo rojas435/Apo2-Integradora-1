39ecf52fd183462f2b6dff24f56c556afa666ff5
juanex2001 <juanseg201@gmail.com>
2023-03-04 13:46:57 -0500

    Changed Node into box, eliminated box class cause it was unnecesary and changed all methods that used node to box

--- src/model/Box.java
+++ src/model/Box.java
@@ -1,13 +1,70 @@
-package model;
+package model; 
 
-public class Box {
-    public int id;
-    public Box next;
-    public Box previous;
+public class Box{
 
-    public Box(int id) {
-        this.id = id;
-        this.next = null;
-        this.previous = null;
-    }
+	private int id;
+    private String display;
+	private Box next;
+	private Box previous;
+	private Box up;
+    private Box down;
+
+	public Box(int value) {
+		this.id = value;
+        this.display = "[" + value + "]" ;
+	}
+
+	public String getDisplay() {
+		return display;
+	}
+
+	public void setDisplay(String display) {
+		this.display = display;
+	}
+
+	public Box getDown() {
+		return down;
+	}
+
+	public void setDown(Box down) {
+		this.down = down;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Box getNext() {
+		return next;
+	}
+
+	public void setNext(Box next) {
+		this.next = next;
+	}
+
+	public Box getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(Box previous) {
+		this.previous = previous;
+	}
+
+	public Box getUp() {
+		return up;
+	}
+
+	public void setUp(Box up) {
+		this.up = up;
+	}
+
+	@Override
+	public String toString(){
+		return "My value is: " + id; 
+	}
+
 }
