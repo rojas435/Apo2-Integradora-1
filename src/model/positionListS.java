4dfb153ca19dc0842d59c897e85b4328fbae4625
juanex2001 <juanseg201@gmail.com>
2023-03-06 13:05:32 -0500

    edited menu, need to change arraylist to node list

---
+++ src/model/positionListS.java
@@ -0,0 +1,22 @@
+package model;
+
+public class positionListS {
+    private Node head;
+    private Node tail;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+}
